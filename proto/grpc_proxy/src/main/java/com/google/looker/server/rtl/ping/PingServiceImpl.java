e8354e3ca801d69a60446e11f03a66019aa5c888
Bryn Ryans <bryn.ryans@looker.com>
2021-01-16 15:26:14 -0800

    tweaks

--- proto/grpc_proxy/src/main/java/com/google/looker/server/rtl/ping/PingServiceImpl.java
+++ proto/grpc_proxy/src/main/java/com/google/looker/server/rtl/ping/PingServiceImpl.java
@@ -1,6 +1,8 @@
 package com.google.looker.server.rtl.ping;
 
-import com.google.looker.server.rtl.ping.PingServiceGrpc.PingServiceImplBase;
+import com.google.looker.server.rtl.PingRequest;
+import com.google.looker.server.rtl.PingResponse;
+import com.google.looker.server.rtl.PingServiceGrpc.PingServiceImplBase;
 import io.grpc.stub.StreamObserver;
 
 public class PingServiceImpl extends PingServiceImplBase {
